cc26d96c30717d0adee864a343ef95a9b118eb75
lindong zhao <lindong4067@sina.com>
2023-02-16 20:38:36 +0800

    dubbo sample init

---
+++ src/main/java/com/example/dubbosample/DubboSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.dubbosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboSampleApplication.class, args);
+    }
+
+}
